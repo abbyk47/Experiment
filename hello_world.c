aed18f6fba0b76df9a31903dba5683f62c035f60
abbyk47 <98438298+abbyk47@users.noreply.github.com>
2022-02-08 16:28:27 -0500

    Add hello_world.c
    An old 'hello world' project from my first coding class.

---
+++ hello_world.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main() {                   //introduces "main", which has no arguments. uses { to start program.
+    printf("Hello, world!\n"); // printf tells to print, words inside quotations tell what to print
+}                              // closing bracket to end code
