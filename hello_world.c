bbf525114f1125f17ecd18291770d43c2c2bbbb0
abbyk47 <98438298+abbyk47@users.noreply.github.com>
2022-02-08 16:35:54 -0500

    Remove Comments
    Removes long, wordy comments, adds a short descriptor at top of code instead.

--- hello_world.c
+++ hello_world.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
+//prints the phrase "Hello, world!" 
 
-int main() {                   //introduces "main", which has no arguments. uses { to start program.
-    printf("Hello, world!\n"); // printf tells to print, words inside quotations tell what to print
-}                              // closing bracket to end code
+int main() {                   
+    printf("Hello, world!\n"); 
+}                              
